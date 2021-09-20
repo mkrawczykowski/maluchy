f99fe4900051a08c728bfe434e86b159e2ec14fd
Michał Krawczykowski <mkrawczykowski@gmail.com>
2021-09-20 12:46:39 +0200

    header HTML structure in index.html

--- index.php
+++ index.php
@@ -2,10 +2,16 @@
   get_header();
 ?>
 
-<div class="posts">
+<!-- <div class="posts">
   <button class="posts__button" id="posts__button">Wczytaj posty</button>
   <div class="posts__list" id="posts__list"></div>
-</div>
+</div> -->
+<!-- test
+<?php if ( is_active_sidebar( 'home_right_1' ) ) : ?>
+	<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
+		<?php dynamic_sidebar( 'home_right_1' ); ?>
+	</div>
+<?php endif; ?> -->
 
 <?php
   get_footer();
--- js/components/scripts.js
+++ js/components/scripts.js
@@ -1,26 +1,26 @@
-console.log('działa JS');
+// console.log('działa JS');
 
-let postsButton = document.getElementById('posts__button');
-let postsList = document.getElementById('posts__list');
+// let postsButton = document.getElementById('posts__button');
+// let postsList = document.getElementById('posts__list');
 
-if (postsButton) {
-  postsButton.addEventListener("click", function () {
-    var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'http://localhost/maluchy/wp-json/wp/v2/posts');
-    ourRequest.onload = function () {
-      if (ourRequest.status >= 200 && ourRequest.status < 400) {
-        var data = JSON.parse(ourRequest.responseText);
-        console.log(data[0].title);
-        postsList.innerHTML = data[0].title.rendered;
-      } else {
-        console.log("We connected to the server, but it returned an error.");
-      }
-    };
+// if (postsButton) {
+//   postsButton.addEventListener("click", function () {
+//     var ourRequest = new XMLHttpRequest();
+//     ourRequest.open('GET', 'http://localhost/maluchy/wp-json/wp/v2/posts');
+//     ourRequest.onload = function () {
+//       if (ourRequest.status >= 200 && ourRequest.status < 400) {
+//         var data = JSON.parse(ourRequest.responseText);
+//         console.log(data[0].title);
+//         postsList.innerHTML = data[0].title.rendered;
+//       } else {
+//         console.log("We connected to the server, but it returned an error.");
+//       }
+//     };
 
-    ourRequest.onerror = function () {
-      console.log("Connection error");
-    };
+//     ourRequest.onerror = function () {
+//       console.log("Connection error");
+//     };
 
-    ourRequest.send();
-  });
-}
+//     ourRequest.send();
+//   });
+// }
